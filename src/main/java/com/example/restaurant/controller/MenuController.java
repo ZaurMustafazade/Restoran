c796f8c499c471d3f30dd571dbecddd13f845322
Zaur.Mustafazade <zaurmustafazade98@mail.ru>
2022-05-30 22:08:47 +0400

    commit

--- src/main/java/com/example/restaurant/controller/MenuController.java
+++ src/main/java/com/example/restaurant/controller/MenuController.java
@@ -2,18 +2,22 @@ package com.example.restaurant.controller;
 
 import com.example.restaurant.model.dto.MenuDto;
 import com.example.restaurant.service.MenuService;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/menu")
+@Api(tags = "menus")
 @RequiredArgsConstructor
 public class MenuController {
     private final MenuService menuService;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping
     public ResponseEntity<List<MenuDto>> findAll() {
         return ResponseEntity.ok(menuService.getAll());
---
+++ src/main/java/com/example/restaurant/model/dto/UserDataDTO.java
@@ -0,0 +1,21 @@
+package com.example.restaurant.model.dto;
+
+import com.example.restaurant.model.enums.AppUserRole;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class UserDataDTO {
+    @ApiModelProperty(position = 0)
+    private String username;
+    @ApiModelProperty(position = 1)
+    private String email;
+    @ApiModelProperty(position = 2)
+    private String password;
+    @ApiModelProperty(position = 3)
+    List<AppUserRole> appUserRoles;
+}
